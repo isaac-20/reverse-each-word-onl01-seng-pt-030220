acdcd7863a67c5d619d3e1fb2a98f229a24c5464
Isaac Smith <isaacsmith215@aol.com>
2020-03-14 00:07:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   rev_array = []
-  rev_array.each do|word|
+  array.each do|word|
     rev_array.push(word.reverse)
   end
   return rev_array.join(" ")
